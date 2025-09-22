4744f3430bb68c3416805e940946d365dcad13d5
rahulkandwal707 <rahulkandwal707@gmail.com>
2025-09-22 18:32:40 +0000

    feat: added cors

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import tasksRouter from './tasks';
+import cors from 'cors';
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.use('/tasks', tasksRouter);
 
